70b00e19852fcf8deea81a131de74fb29f17f439
mkadr5 <muhammetkadir52@gmail.com>
2023-03-25 00:46:52 +0300

    Initial Commit

---
+++ PhotoPickerSwiftUI/PhotoPickerSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoPickerSwiftUIApp.swift
+//  PhotoPickerSwiftUI
+//
+//  Created by Muhammet Kadir on 25.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct PhotoPickerSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
